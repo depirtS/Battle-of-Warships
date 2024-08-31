c812d528bc97abec0d8eff09b73627f32b71944d
depirtS <karol.fol@onet.pl>
2024-08-31 17:55:29 +0200

    Added structure for HowToPlay page

--- WarshipsOnline/HowToPlay.xaml.cs
+++ WarshipsOnline/HowToPlay.xaml.cs
@@ -2,15 +2,37 @@ namespace WarshipsOnline;
 
 public partial class HowToPlay : ContentPage
 {
-	public HowToPlay()
+    protected override bool OnBackButtonPressed()
+    {
+        GlobalManager.LoadingOverlay(LoadingOverlay, Navigation);
+        return true;
+    }
+    public HowToPlay()
 	{
         GlobalManager.SetCulture(GlobalManager.CultureCode);
         InitializeComponent();
+        MainGrid_SizeChanged(this, new EventArgs());
         NavigationPage.SetHasNavigationBar(this, false);
         InitializeLanguage();
     }
     private void InitializeLanguage()
     {
+        Header.Text = AppResources.HowPlay;
+    }
 
+    private void MainGrid_SizeChanged(object sender, EventArgs e)
+    {
+        int width = (int)MainGrid.Width;
+        int height = (int)MainGrid.Height;
+        if (width < height)
+        {
+            GrayField.FontSize = YellowField.FontSize = RedField.FontSize = DarkRedField.FontSize = OrangeField.FontSize = TransparentField.FontSize = TransparentButton.FontSize = width / 30;
+            Header.FontSize = width / 20;
+        }
+        else
+        {
+            GrayField.FontSize = YellowField.FontSize = RedField.FontSize = DarkRedField.FontSize = OrangeField.FontSize = TransparentField.FontSize = TransparentButton.FontSize = width / 30;
+            Header.FontSize = width / 20;
+        }
     }
 }
